ec0fdbe907bd50417affcb70af3bacef0d355280
neeraj <neerajmail888@gmail.com>
2024-04-07 00:45:53 +0530

    app

--- app.py
+++ app.py
@@ -29,3 +29,18 @@ def input_image_setup(uploaded_file):
         return image_parts
     else:
         raise FileNotFoundError("No file uploaded")
+    
+    
+    
+st.set_page_config(page_title="Gemini Health App")
+
+st.header("Gemini Health App")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me the total calories")
