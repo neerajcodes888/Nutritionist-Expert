2da4f86f120be6965d15ffe8f1bdf67d5cce9be1
neeraj <neerajmail888@gmail.com>
2024-04-07 00:44:55 +0530

    file

--- app.py
+++ app.py
@@ -11,3 +11,21 @@ def get_gemini_repsonse(input,image,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
     response=model.generate_content([input,image[0],prompt])
     return response.text
+
+
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
