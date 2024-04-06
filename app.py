44bc7e156a46e7100f8a7740034f631d8eb8b337
neeraj <neerajmail888@gmail.com>
2024-04-07 00:44:24 +0530

    response

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ from PIL import Image
 from dotenv import load_dotenv
 
 load_dotenv() 
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
