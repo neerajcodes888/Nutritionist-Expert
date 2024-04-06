7ef57d11c006a7179fce8687c371a3822f7e7d2d
neeraj <neerajmail888@gmail.com>
2024-04-07 00:47:49 +0530

    submit

--- app.py
+++ app.py
@@ -44,3 +44,24 @@ if uploaded_file is not None:
 
 
 submit=st.button("Tell me the total calories")
+
+
+input_prompt="""
+You are an expert in nutritionist where you need to see the food items from the image
+               and calculate the total calories, also provide the details of every food items with calories intake
+               is below format
+
+               1. Item 1 - no of calories
+               2. Item 2 - no of calories
+               ----
+               ----
+
+
+"""
+
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
