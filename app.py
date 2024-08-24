38bd34dcdf97f1b540d828d1ac47260fb0a3aaa2
Neeraj Kumar <neerajmail888@gmail.com>
2024-08-24 11:37:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv() 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 def get_gemini_repsonse(input,image,prompt):
-    model=genai.GenerativeModel('gemini-pro-vision')
+    model=genai.GenerativeModel('gemini-1.5-flash')
     response=model.generate_content([input,image[0],prompt])
     return response.text
 
